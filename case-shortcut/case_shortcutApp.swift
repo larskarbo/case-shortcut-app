ef83ecd1d7d19db7802d8351b1f7f442df159fb3
Lars Karbo <mail@larskarbo.no>
2021-02-23 15:59:37 +0100

    Initial Commit

---
+++ case-shortcut/case_shortcutApp.swift
@@ -0,0 +1,17 @@
+//
+//  case_shortcutApp.swift
+//  case-shortcut
+//
+//  Created by Lars Karbø on 23/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct case_shortcutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
